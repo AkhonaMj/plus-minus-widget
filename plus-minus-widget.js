7306279bf2b6d2f1435ade424fbfa7d85b9773b1
AkhonaMj <akhona0304mjikelo@gmail.com>
2023-04-26 13:06:51 +0200

    first commit

---
+++ plus-minus-widget.js
@@ -0,0 +1,21 @@
+const addBtn  = document.querySelector(".plus")
+const minusBtn  = document.querySelector(".minus")
+
+function addBtnClicked() {
+    var total = 0;
+    var buttonSelected = 0;
+    // if (buttonSelected == addBtn){
+    //     total += 1
+    // }
+    alert('hi')
+}
+
+addBtn.addEventListener("click", addBtnClicked)
+
+
+
+
+function minusBtnClicked(){
+
+}
+minusBtn.addEventListener("click", )
