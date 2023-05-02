76c857ec997026c298e6653e4d30df22d9c74f94
AkhonaMj <akhona0304mjikelo@gmail.com>
2023-05-02 13:02:01 +0200

    made changes

--- plus-minus-widget.js
+++ plus-minus-widget.js
@@ -1,9 +1,11 @@
 const addBtn  = document.querySelector(".plus");
 const minusBtn  = document.querySelector(".minus");
 const  totalValue = document.querySelector(".total-value");
-const refreshBtn = document.querySelector(".refresh")
-
+const refreshBtn = document.querySelector(".refresh");
+const multiplyBtn = document.querySelector(".multiply");
+const  textBox = document.querySelector(".number-value")
 var total = 0;
+
 
 
 function addBtnClicked() {
@@ -39,4 +41,10 @@ addBtn.addEventListener("click", addBtnClicked)
  refreshBtn.addEventListener("click", function refreshBtnClicked(){
     totalValue.innerHTML = 0;
     location.reload() 
+
+ });
+
+ multiplyBtn.addEventListener("click", function multiplyBtnClicked(){
+    totalValue.innerHTML = textBox.value * total;
+
  });
